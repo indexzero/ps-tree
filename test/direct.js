27f5301934a7f67cc1d2d16be04e0c02b87753cf
simonepri <simonepri@outlook.com>
2018-03-15 10:03:56 +0100

    Improve test suite

--- test/direct.js
+++ test/direct.js
@@ -2,8 +2,7 @@ var test  = require('tape');
 var chalk = require('chalk');
 var cp = require('child_process');
 
-var red = chalk.red,
-    green = chalk.green,
+var green = chalk.green,
     cyan = chalk.cyan;
 
 test(cyan('Directly Execute bin/ps-tree.js'), function (t) {
@@ -11,7 +10,9 @@ test(cyan('Directly Execute bin/ps-tree.js'), function (t) {
   var child = cp.exec('node ./bin/ps-tree.js', function (error, data) {
     // console.log('data: ' + data.length);
     if (error !== null) {
-      console.log(red('exec error: ' + error));
+      t.error(err);
+      t.end();
+      return;
     }
   });
 
--- test/test.js
+++ test/test.js
@@ -5,8 +5,7 @@ var cp = require('child_process');
 var treeKill = require('tree-kill');
 var psTree = require('../');
 
-var red = chalk.red,
-    green = chalk.green,
+var green = chalk.green,
     cyan = chalk.cyan;
 
 var scripts = {
@@ -15,56 +14,90 @@ var scripts = {
 };
 
 test(cyan('Spawn a Parent process which has a Two Child Processes'), function (t) {
+  t.timeoutAfter(5000);
   var parent = cp.exec('node ' + scripts.parent, function (error, stdout, stderr) {});
 
-  setTimeout(function () {
-    psTree(parent.pid, function (err, children) {
-      if (err) { console.log(err); }
-      console.log(red('Children: '), children, '\n');
-      t.true(children.length > 0, green('✓ There are ' + children.length + ' active child processes'));
-      treeKill(parent.pid);
-    });
+  var started = false;
+  parent.stdout.on('data', (data) => {
+    if (started) return;
+    started = true;
 
-    setTimeout(function () {
-      psTree(parent.pid, function (err, children) {
-        if (err) { console.log(err); }
-        // console.log('Children: ', children, '\n');
-        // console.log(' ')
-        t.equal(children.length, 0, green('✓ No more active child processes (we killed them)'));
+    psTree(parent.pid, function (err, children) {
+      if (err) {
+        t.error(err);
         t.end();
+        return;
+      }
+
+      t.true(children.length > 0, green('✓ There are ' + children.length + ' active child processes'));
+
+      treeKill(parent.pid, function(err) {
+        if (err) {
+          t.error(err);
+          t.end();
+          return;
+        }
+
+        psTree(parent.pid, function (err, children) {
+          if (err) {
+            t.error(err);
+            t.end();
+            return;
+          }
+
+          t.equal(children.length, 0, green('✓ No more active child processes (we killed them)'));
+          t.end();
+        });
       });
-    }, 2000); // give psTree time to kill the processes
-  }, 500); // give the child process time to spawn
-  // need more time on a slow(or heavy load server). maybe promise.then is better instead of the timeout
+    });
+  });
 });
 
 test(cyan('FORCE ERROR by calling psTree without supplying a Callback'), function (t) {
-  var errmsg = 'Error: childrenOfPid(pid, callback) expects callback'
+  var errmsg = 'Error: childrenOfPid(pid, callback) expects callback';
+
   // Attempt to call psTree without a callback
   try { psTree(1234); }
   catch (e) {
-    t.equal(e.toString(), errmsg, green('✓ Fails when no callback supplied (as expected)'))
+    t.equal(e.toString(), errmsg, green('✓ Fails when no callback supplied (as expected)'));
   }
 
   t.end();
 });
 
-
 test(cyan('Spawn a Child Process and psTree with a String as pid'), function (t) {
+  t.timeoutAfter(5000);
   var child = cp.exec('node ' + scripts.child, function(error, stdout, stderr) {});
-  setTimeout(function(){
-    psTree(child.pid.toString(), function (err, children) {
-      if (err) { console.log(err); }
-      // cp.spawn('kill', ['-9'].concat(children.map(function (p) { return p.PID })))
-      treeKill(child.pid);
-    });
 
-    setTimeout(function() {
-      psTree(child.pid.toString(), function (err, children) {
-        if (err) { console.log(err); }
-        t.equal(children.length, 0, green('✓ No more active child processes'));
+  var started = false;
+  child.stdout.on('data', (data) => {
+    if (started) return;
+    started = true;
+
+    psTree(child.pid, function (err, children) {
+      if (err) {
+        t.error(err);
         t.end();
+        return;
+      }
+      treeKill(child.pid, function(err) {
+        if (err) {
+          t.error(err);
+          t.end();
+          return;
+        }
+
+        psTree(child.pid, function (err, children) {
+          if (err) {
+            t.error(err);
+            t.end();
+            return;
+          }
+
+          t.equal(children.length, 0, green('✓ No more active child processes'));
+          t.end();
+        });
       });
-    }, 1000); // give psTree time to kill the processes
-  }, 200); // give the child process time to spawn
+    });
+  });
 });
